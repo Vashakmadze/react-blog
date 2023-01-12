196396a79eff2cce833d432d6140eade338e373c
Luka Vashakmadze <50746021+Vashakmadze@users.noreply.github.com>
2023-01-12 21:39:54 +0100

    fixed react router navigation bug

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,10 +10,10 @@ const Header = () => (
     <div className='title'>Liza's Blog</div>
     <nav className='navigation'>
       <ul>
-        <li><NavLink to={`/home`}>home</NavLink></li>
-        <li><NavLink to={`/food`}>food</NavLink></li>
-        <li><NavLink to={`/mind`}>mind</NavLink></li>
-        <li><NavLink to={`/selfcare`}>selfcare</NavLink></li>
+        <li><NavLink to={`/react-blog/home`}>home</NavLink></li>
+        <li><NavLink to={`/react-blog/food`}>food</NavLink></li>
+        <li><NavLink to={`/react-blog/mind`}>mind</NavLink></li>
+        <li><NavLink to={`/react-blog/selfcare`}>selfcare</NavLink></li>
       </ul>
     </nav>
     <div className='search'>
