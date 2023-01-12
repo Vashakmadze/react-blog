69bb27b322b34a0552e2b4b2b325499364884508
Luka Vashakmadze <50746021+Vashakmadze@users.noreply.github.com>
2023-01-12 10:34:24 +0100

    removed feature state

--- src/components/BlogPage/BlogPage.js
+++ src/components/BlogPage/BlogPage.js
@@ -2,16 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './BlogPage.css';
 import { useLocation } from 'react-router-dom'
+import ReactHtmlParser from 'react-html-parser';
 
 
 function BlogPage() {
   const location = useLocation()
   const data = location.state;
-  console.log(data);
   return (
     <>
     <div className="BlogPage">
-      BlogPage Component
+      <img className='coverImageBlog' src={data.image} alt="Blog cover media" title="Blog cover media" />
+      <div className='postBlog'>
+        <h1 className='pageTitle'>{data.title}</h1>
+        <div className='input'>
+          {ReactHtmlParser(data.html)}
+        </div>
+      </div>
     </div>
     </>
   );
